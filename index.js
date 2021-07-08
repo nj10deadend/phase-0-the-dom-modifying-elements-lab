208a52e4a8d0986477804e9a679ac83a91c40ca7
nj10deadend <nasjacks10@gmail.com>
2021-07-08 15:43:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector('main'); // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
